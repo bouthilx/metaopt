Epistimio/orion

Client: Add helper function for manual insertion of parameter sets
Why:
A user might want to populate a collection of trials with one's own
choices of set of parameters for a particular experiment. Call it
intuition, call it bias, call it experience; the user should be able to
act as if one was the Producer itself.

How:
User can either write a second script of one's own, where some tuples of
appropriate sets of parameters are calculated and fed to
`metaopt.client.insert_trials`, or one can do the same thing from
iPython - for example. The function is written as part of the
`metaopt.client` interface to facilitate this interaction.

In order to work, an `Experiment` object must be instantiated, and this
need in turn a `Database` object to lie in the Python process. So user
is asked to pass necessarily the experiment's name which the trials will
be associated with, and take care of a way to discover the Database. The
latter will be done in the same manner that `metaopt.core.cli.infer_experiment`
discovers the `Database`. That is either by default locations,
environmental variables, or an external configuration. The last one is
enabled by allowing an extra optional keyword argument in the helper
function, ``cmdconfig``, which is expected to have the key 'database'
which is a dictionary with configuration details, as usual.

Note:
The experiment must exist already as a configuration in the specified
database. **So, currently pre-population of an experiment's new trials is
not supported**.

Finally:
All points suggested by the user are checked against the experiment's
parameter space. In case of error, an optional argument controls whether
an `AssertionError` will be raised or the failing parameter sets will be
ignored.

src/orion/client/__init__.py
@@ -10,6 +10,9 @@
 """
 import os
 
+from metaopt.client.manual import insert_trials  # noqa
+
+
 IS_METAOPT_ON = False
 _HAS_REPORTED_RESULTS = False
 RESULTS_FILENAME = os.getenv('METAOPT_RESULTS_PATH', None)

src/metaopt/client/manual.py
@@ -1,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+:mod:`metaopt.client.manual` -- Routines for manual interaction with database
+=============================================================================
+
+.. module:: manual
+   :platform: Unix
+   :synopsis: Provides a simple interface to log new trials into the database
+      and link them with a particular existing experiment.
+
+"""
+from metaopt.core import resolve_config
+from metaopt.core.io.database import Database
+from metaopt.core.utils import (format_trials, SingletonError,)
+from metaopt.core.worker.experiment import Experiment
+
+
+def insert_trials(experiment_name, points, cmdconfig=None, raise_exc=True):
+    """Insert sets of parameters manually, defined in `points`, as new trials
+    for the experiment name, `experiment_name`.
+
+    :param experiment_name: Name of the experiment which the new trials are
+       going to be associated with
+    :param points: list of tuples in agreement with experiment's parameter space
+    :param raise_exc: whether an inappropriate tuple of parameters will raise
+       an exception or it will be ignored
+
+    .. info:: If `raise_exc` is True, no set of parameters will be inserted. If
+       it is False, only the valid ones will be inserted; the rest will be ignored.
+
+    .. note:: This cannot be used to prepopulate a future experiment. So,
+       an experiment with `experiment_name` should already be configured in
+       the database.
+
+    """
+    cmdconfig = cmdconfig if cmdconfig else {}
+    config = resolve_config.fetch_default_options()  # Get database perhaps from default locs
+    config = resolve_config.merge_env_vars(config)  # Get database perhaps from env vars
+
+    tmpconfig = resolve_config.merge_mopt_config(config, dict(),
+                                                 cmdconfig, dict())
+
+    db_opts = tmpconfig['database']
+    db_type = db_opts.pop('type')
+    try:
+        Database(of_type=db_type, **db_opts)
+    except SingletonError:
+        pass
+
+    experiment = Experiment(experiment_name)
+    # Configuration is completely taken from the database
+    if experiment.status is None:
+        raise ValueError("No experiment named '{}' could be found.".format(experiment_name))
+    experiment.configure(experiment.configuration)
+
+    valid_points = []
+    for point in points:
+        try:
+            assert point in experiment.space
+            valid_points.append(point)
+        except AssertionError:
+            if raise_exc:
+                raise
+
+    if not valid_points:
+        return
+
+    new_trials = list(map(lambda data: format_trials.tuple_to_trial(data,
+                                                                    experiment.space),
+                          valid_points))
+    experiment.register_trials(new_trials)

tests/unittests/client/test_manual.py
@@ -1,0 +1,136 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Example usage and tests for :mod:`metaopt.client.manual`."""
+
+import pytest
+
+from metaopt.client import insert_trials
+from metaopt.core.worker.experiment import Experiment
+
+
+@pytest.mark.usefixtures("null_db_instances", "clean_db", "with_user_tsirif")
+def test_good_insertion_exp_exists(monkeypatch, database, random_dt):
+    """Check if a correct insertion to an existing experiment is done correctly."""
+    monkeypatch.setenv('METAOPT_DB_NAME', 'metaopt_test')
+    monkeypatch.setenv('METAOPT_DB_ADDRESS', 'mongodb://user:pass@localhost')
+
+    count_before = database.trials.count()
+    insert_trials('supernaedo2', [('rnn', 'gru'), ('lstm', 'lstm_with_attention')])
+    insert_trials('supernaedo3', [('rnn', 'rnn')])
+
+    assert database.trials.count() == count_before + 3
+    trials = list(database.trials.find())
+    exp1 = Experiment('supernaedo2')
+    exp2 = Experiment('supernaedo3')
+
+    assert trials[-3]['experiment'] == exp1._id
+    assert trials[-3]['status'] == 'new'
+    assert trials[-3]['submit_time'] == random_dt
+    assert len(trials[-3]['params']) == 2
+    assert trials[-3]['params'][0] == {
+        'name': '/encoding_layer',
+        'type': 'categorical',
+        'value': 'rnn'
+        }
+    assert trials[-3]['params'][1] == {
+        'name': '/decoding_layer',
+        'type': 'categorical',
+        'value': 'gru'
+        }
+
+    assert trials[-2]['experiment'] == exp1._id
+    assert trials[-2]['status'] == 'new'
+    assert trials[-2]['submit_time'] == random_dt
+    assert len(trials[-2]['params']) == 2
+    assert trials[-2]['params'][0] == {
+        'name': '/encoding_layer',
+        'type': 'categorical',
+        'value': 'lstm'
+        }
+    assert trials[-2]['params'][1] == {
+        'name': '/decoding_layer',
+        'type': 'categorical',
+        'value': 'lstm_with_attention'
+        }
+
+    assert trials[-1]['experiment'] == exp2._id
+    assert trials[-1]['status'] == 'new'
+    assert trials[-1]['submit_time'] == random_dt
+    assert len(trials[-1]['params']) == 2
+    assert trials[-1]['params'][0] == {
+        'name': '/encoding_layer',
+        'type': 'categorical',
+        'value': 'rnn'
+        }
+    assert trials[-1]['params'][1] == {
+        'name': '/decoding_layer',
+        'type': 'categorical',
+        'value': 'rnn'
+        }
+
+
+@pytest.mark.usefixtures("null_db_instances", "clean_db", "with_user_tsirif")
+def test_bad_insertion_exp_exists_ignores(database, random_dt):
+    """Check if a bad insertion to an existing experiment will be ignored.
+
+    The ones that are correct will be inserted.
+    """
+    config = dict(database={'name': 'metaopt_test',
+                            'type': 'mongodb',
+                            'host': 'mongodb://user:pass@localhost'})
+
+    count_before = database.trials.count()
+    insert_trials('supernaedo2', [('rnn', 5), ('lstm', 'lstm_with_attention')],
+                  cmdconfig=config, raise_exc=False)
+    insert_trials('supernaedo3', [(('lalal', 'ispii'), 'rnn')],
+                  raise_exc=False)
+    assert database.trials.count() == count_before + 1
+
+    trials = list(database.trials.find())
+    exp1 = Experiment('supernaedo2')
+
+    assert trials[-1]['experiment'] == exp1._id
+    assert trials[-1]['status'] == 'new'
+    assert trials[-1]['submit_time'] == random_dt
+    assert len(trials[-1]['params']) == 2
+    assert trials[-1]['params'][0] == {
+        'name': '/encoding_layer',
+        'type': 'categorical',
+        'value': 'lstm'
+        }
+    assert trials[-1]['params'][1] == {
+        'name': '/decoding_layer',
+        'type': 'categorical',
+        'value': 'lstm_with_attention'
+        }
+
+
+@pytest.mark.usefixtures("null_db_instances", "clean_db", "with_user_tsirif")
+def test_bad_insertion_exp_exists_raises(monkeypatch, database):
+    """Check if a correct insertion to an existing experiment raises an exception.
+
+    No set will be inserted
+    """
+    monkeypatch.setenv('METAOPT_DB_NAME', 'metaopt_test')
+    monkeypatch.setenv('METAOPT_DB_ADDRESS', 'mongodb://user:pass@localhost')
+
+    count_before = database.trials.count()
+    with pytest.raises(AssertionError):
+        insert_trials('supernaedo2', [('rnn', 5), ('lstm', 'lstm_with_attention')],
+                      raise_exc=True)
+    with pytest.raises(AssertionError):
+        insert_trials('supernaedo3', [(('lalal', 'ispii'), 'rnn')])
+    assert database.trials.count() == count_before
+
+
+@pytest.mark.usefixtures("null_db_instances", "clean_db", "with_user_tsirif")
+def test_insertion_new_exp(monkeypatch, database):
+    """Check that it raises if a new experiment name is given."""
+    monkeypatch.setenv('METAOPT_DB_NAME', 'metaopt_test')
+    monkeypatch.setenv('METAOPT_DB_ADDRESS', 'mongodb://user:pass@localhost')
+
+    count_before = database.trials.count()
+    with pytest.raises(ValueError) as exc:
+        insert_trials('supernaedo', [('rnn', 'rnn')])
+    assert "No experiment named" in str(exc.value)
+    assert database.trials.count() == count_before

bouthilx/metaopt

src/orion/client/__init__.py
@@ -10,6 +10,8 @@
 """
 import os
 
+from orion.client.manual import insert_trials  # noqa
+
 IS_ORION_ON = False
 _HAS_REPORTED_RESULTS = False
 RESULTS_FILENAME = os.getenv('ORION_RESULTS_PATH', None)

src/orion/client/manual.py
@@ -1,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+:mod:`orion.client.manual` -- Routines for manual interaction with database
+===========================================================================
+
+.. module:: manual
+   :platform: Unix
+   :synopsis: Provides a simple interface to log new trials into the database
+      and link them with a particular existing experiment.
+
+"""
+from orion.core import resolve_config
+from orion.core.io.database import Database
+from orion.core.utils import (format_trials, SingletonError,)
+from orion.core.worker.experiment import Experiment
+
+
+def insert_trials(experiment_name, points, cmdconfig=None, raise_exc=True):
+    """Insert sets of parameters manually, defined in `points`, as new trials
+    for the experiment name, `experiment_name`.
+
+    :param experiment_name: Name of the experiment which the new trials are
+       going to be associated with
+    :param points: list of tuples in agreement with experiment's parameter space
+    :param raise_exc: whether an inappropriate tuple of parameters will raise
+       an exception or it will be ignored
+
+    .. note:: If `raise_exc` is True, no set of parameters will be inserted. If
+       it is False, only the valid ones will be inserted; the rest will be ignored.
+
+    .. note:: This cannot be used to prepopulate a future experiment. So,
+       an experiment with `experiment_name` should already be configured in
+       the database.
+
+    """
+    cmdconfig = cmdconfig if cmdconfig else {}
+    config = resolve_config.fetch_default_options()  # Get database perhaps from default locs
+    config = resolve_config.merge_env_vars(config)  # Get database perhaps from env vars
+
+    tmpconfig = resolve_config.merge_orion_config(config, dict(),
+                                                 cmdconfig, dict())
+
+    db_opts = tmpconfig['database']
+    db_type = db_opts.pop('type')
+    try:
+        Database(of_type=db_type, **db_opts)
+    except SingletonError:
+        pass
+
+    experiment = Experiment(experiment_name)
+    # Configuration is completely taken from the database
+    if experiment.status is None:
+        raise ValueError("No experiment named '{}' could be found.".format(experiment_name))
+    experiment.configure(experiment.configuration)
+
+    valid_points = []
+    for point in points:
+        try:
+            assert point in experiment.space
+            valid_points.append(point)
+        except AssertionError:
+            if raise_exc:
+                raise
+
+    if not valid_points:
+        return
+
+    new_trials = list(map(lambda data: format_trials.tuple_to_trial(data,
+                                                                    experiment.space),
+                          valid_points))
+    experiment.register_trials(new_trials)

tests/unittests/client/test_manual.py
@@ -1,0 +1,136 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Example usage and tests for :mod:`orion.client.manual`."""
+
+import pytest
+
+from orion.client import insert_trials
+from orion.core.worker.experiment import Experiment
+
+
+@pytest.mark.usefixtures("null_db_instances", "clean_db", "with_user_tsirif")
+def test_good_insertion_exp_exists(monkeypatch, database, random_dt):
+    """Check if a correct insertion to an existing experiment is done correctly."""
+    monkeypatch.setenv('ORION_DB_NAME', 'orion_test')
+    monkeypatch.setenv('ORION_DB_ADDRESS', 'mongodb://user:pass@localhost')
+
+    count_before = database.trials.count()
+    insert_trials('supernaedo2', [('rnn', 'gru'), ('lstm', 'lstm_with_attention')])
+    insert_trials('supernaedo3', [('rnn', 'rnn')])
+
+    assert database.trials.count() == count_before + 3
+    trials = list(database.trials.find())
+    exp1 = Experiment('supernaedo2')
+    exp2 = Experiment('supernaedo3')
+
+    assert trials[-3]['experiment'] == exp1._id
+    assert trials[-3]['status'] == 'new'
+    assert trials[-3]['submit_time'] == random_dt
+    assert len(trials[-3]['params']) == 2
+    assert trials[-3]['params'][0] == {
+        'name': '/encoding_layer',
+        'type': 'categorical',
+        'value': 'rnn'
+        }
+    assert trials[-3]['params'][1] == {
+        'name': '/decoding_layer',
+        'type': 'categorical',
+        'value': 'gru'
+        }
+
+    assert trials[-2]['experiment'] == exp1._id
+    assert trials[-2]['status'] == 'new'
+    assert trials[-2]['submit_time'] == random_dt
+    assert len(trials[-2]['params']) == 2
+    assert trials[-2]['params'][0] == {
+        'name': '/encoding_layer',
+        'type': 'categorical',
+        'value': 'lstm'
+        }
+    assert trials[-2]['params'][1] == {
+        'name': '/decoding_layer',
+        'type': 'categorical',
+        'value': 'lstm_with_attention'
+        }
+
+    assert trials[-1]['experiment'] == exp2._id
+    assert trials[-1]['status'] == 'new'
+    assert trials[-1]['submit_time'] == random_dt
+    assert len(trials[-1]['params']) == 2
+    assert trials[-1]['params'][0] == {
+        'name': '/encoding_layer',
+        'type': 'categorical',
+        'value': 'rnn'
+        }
+    assert trials[-1]['params'][1] == {
+        'name': '/decoding_layer',
+        'type': 'categorical',
+        'value': 'rnn'
+        }
+
+
+@pytest.mark.usefixtures("null_db_instances", "clean_db", "with_user_tsirif")
+def test_bad_insertion_exp_exists_ignores(database, random_dt):
+    """Check if a bad insertion to an existing experiment will be ignored.
+
+    The ones that are correct will be inserted.
+    """
+    config = dict(database={'name': 'orion_test',
+                            'type': 'mongodb',
+                            'host': 'mongodb://user:pass@localhost'})
+
+    count_before = database.trials.count()
+    insert_trials('supernaedo2', [('rnn', 5), ('lstm', 'lstm_with_attention')],
+                  cmdconfig=config, raise_exc=False)
+    insert_trials('supernaedo3', [(('lalal', 'ispii'), 'rnn')],
+                  raise_exc=False)
+    assert database.trials.count() == count_before + 1
+
+    trials = list(database.trials.find())
+    exp1 = Experiment('supernaedo2')
+
+    assert trials[-1]['experiment'] == exp1._id
+    assert trials[-1]['status'] == 'new'
+    assert trials[-1]['submit_time'] == random_dt
+    assert len(trials[-1]['params']) == 2
+    assert trials[-1]['params'][0] == {
+        'name': '/encoding_layer',
+        'type': 'categorical',
+        'value': 'lstm'
+        }
+    assert trials[-1]['params'][1] == {
+        'name': '/decoding_layer',
+        'type': 'categorical',
+        'value': 'lstm_with_attention'
+        }
+
+
+@pytest.mark.usefixtures("null_db_instances", "clean_db", "with_user_tsirif")
+def test_bad_insertion_exp_exists_raises(monkeypatch, database):
+    """Check if a correct insertion to an existing experiment raises an exception.
+
+    No set will be inserted
+    """
+    monkeypatch.setenv('ORION_DB_NAME', 'orion_test')
+    monkeypatch.setenv('ORION_DB_ADDRESS', 'mongodb://user:pass@localhost')
+
+    count_before = database.trials.count()
+    with pytest.raises(AssertionError):
+        insert_trials('supernaedo2', [('rnn', 5), ('lstm', 'lstm_with_attention')],
+                      raise_exc=True)
+    with pytest.raises(AssertionError):
+        insert_trials('supernaedo3', [(('lalal', 'ispii'), 'rnn')])
+    assert database.trials.count() == count_before
+
+
+@pytest.mark.usefixtures("null_db_instances", "clean_db", "with_user_tsirif")
+def test_insertion_new_exp(monkeypatch, database):
+    """Check that it raises if a new experiment name is given."""
+    monkeypatch.setenv('ORION_DB_NAME', 'orion_test')
+    monkeypatch.setenv('ORION_DB_ADDRESS', 'mongodb://user:pass@localhost')
+
+    count_before = database.trials.count()
+    with pytest.raises(ValueError) as exc:
+        insert_trials('supernaedo', [('rnn', 'rnn')])
+    assert "No experiment named" in str(exc.value)
+    assert database.trials.count() == count_before