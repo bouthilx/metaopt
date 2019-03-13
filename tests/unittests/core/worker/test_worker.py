0b43757670777faf407662d6d5dab9408b1c2772
Xavier Bouthillier <xavier.bouthillier@umontreal.ca>
2019-03-13 14:56:55 -0400

    Fix --worker-trials
    Why:
    
    The loop in the worker was either producer or consumer, which made
    worker count each production of trials as 1 trial execution. In other
    words, --worker-trials 1 would execute no trial if it produced trials.

---
+++ tests/unittests/core/worker/test_worker.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Collection of tests for :mod:`orion.core.worker`."""
+
+
+def test_worker_trials():
+    """Test whether the worker will execute the number of trials specified."""
+    assert False
