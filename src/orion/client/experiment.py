a40c7a95334e65d9187791c88d571fa20fb5c27d
Thomas Schweizer <thomas.schweizer@mila.quebec>
2020-05-22 14:57:20 -0400

    Add API endpoints and basic structure

--- src/orion/client/experiment.py
+++ src/orion/client/experiment.py
@@ -22,6 +22,7 @@ import orion.core.worker
 from orion.core.worker.trial import Trial
 from orion.core.worker.trial_pacemaker import TrialPacemaker
 from orion.storage.base import FailedUpdate
+from orion.plotting import PlotAccessor
 
 
 log = logging.getLogger(__name__)
@@ -61,7 +62,9 @@ class ExperimentClient:
         if heartbeat is None:
             heartbeat = orion.core.config.worker.heartbeat
         self.heartbeat = heartbeat
+        self.plot = PlotAccessor(self)
         atexit.register(self.set_broken_trials)
+
 
     ###
     # Attributes
